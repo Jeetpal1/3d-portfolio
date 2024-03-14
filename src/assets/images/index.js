eb8386d57e71f418709a776fbeb5a2cc0ddf1219
Jeetpal Singh <70360391+Jeetpal1@users.noreply.github.com>
2024-03-14 01:23:43 -0500

    Add files via upload

---
+++ src/assets/images/index.js
@@ -0,0 +1,8 @@
+import hero from "./hero.jpg";
+import jeetpalPic from "./JeetpalSingh.jpg";
+import meta from "./meta.png";
+import shopify from "./shopify.png";
+import starbucks from "./starbucks.png";
+import tesla from "./tesla.png";
+
+export { hero, jeetpalPic, meta, shopify, starbucks, tesla };
