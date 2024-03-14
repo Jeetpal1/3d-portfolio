a6003b368887015c5661167706fcd1b043974253
Jeetpal Singh <70360391+Jeetpal1@users.noreply.github.com>
2024-03-14 01:27:49 -0500

    Update index.js

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,8 +1,7 @@
-import hero from "./hero.jpg";
 import jeetpalPic from "./JeetpalSingh.jpg";
 import meta from "./meta.png";
 import shopify from "./shopify.png";
 import starbucks from "./starbucks.png";
 import tesla from "./tesla.png";
 
-export { hero, jeetpalPic, meta, shopify, starbucks, tesla };
+export { jeetpalPic, meta, shopify, starbucks, tesla };
